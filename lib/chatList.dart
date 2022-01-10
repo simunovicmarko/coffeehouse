c6854e98e9930ce1768e19ec02a44e9f81de1932
simunovicmarko <59643387+simunovicmarko@users.noreply.github.com>
2022-01-10 17:12:21 +0100

    Added logout

---
+++ lib/chatList.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: file_names
+import 'package:flutter/material.dart';
+
+class ChatList extends StatefulWidget {
+  const ChatList({Key? key}) : super(key: key);
+
+  @override
+  ChatListState createState() => ChatListState();
+}
+
+class ChatListState extends State<ChatList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
